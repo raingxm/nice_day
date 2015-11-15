c887e554516f7799314d4b7d96f08dcccc84cf78
raingxm <raingxm@163.com>
2015-11-15 21:27:19 +0800

    add parameter for looking for specific course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
-    @search_item = "python"
+    @search_item = params[:look_for] || 'diet'
     @courses = Coursera.for @search_item
     puts @courses
   end
